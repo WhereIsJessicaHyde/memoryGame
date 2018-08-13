7f32ff377781d386fe03273ace1a1da106d699c3
Anjenaex <anjenaex@gmail.com>
2018-08-13 12:01:50 +0200

    primer commit

---
+++ src/memory.js
@@ -0,0 +1,21 @@
+// var MemoryGame = function (cards) {
+//   this.cards = cards;
+// };
+var MemoryGame = function (cards) {
+ this.cards = cards;
+ this.pickedCards = [];
+ this.pairsClicked = [];
+ this.pairsGuessed = [];
+};
+
+// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+
+// };
+
+// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+
+// }
+
+// MemoryGame.prototype.finished = function () {
+
+// };
