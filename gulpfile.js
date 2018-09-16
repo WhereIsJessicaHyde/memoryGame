a2c9782022206616f2b11f6e1ac0f29b5fc73b75
Antonio <anjenaex@gmail.com>
2018-09-16 10:33:11 +0200

    voy a meterle bem y sass al proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,16 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var sass = require('gulp-sass');
+var autoPrefixer = require ('gulp-autoprefixer');
 
 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(autoprefixer())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('stylesheets'));
+});
 
 // watch Sass files for changes, run the Sass preprocessor with the 'sass' task and reload
 gulp.task('server', function() {
@@ -12,15 +20,14 @@ gulp.task('server', function() {
       baseDir: "./"
     }
   });
+   gulp.watch("sass/style.scss", ['sass']).on('change', reload);
    gulp.watch("./css/style.css").on('change', reload);
-   gulp.watch("./src/main.js").on('change', reload);
-   gulp.watch("./src/memory.js").on('change', reload);
+   gulp.watch("./js/main.js").on('change', reload);
+   gulp.watch("./js/memory.js").on('change', reload);
    gulp.watch("./index.html").on("change", reload);
 
 });
 
 
-//Vuelve a ejecutar la tarea cuando se modifica algún archivo 
+//Vuelve a ejecutar la tarea cuando se modifica algún archivo
 gulp.task('default', ['server']);
-
-
