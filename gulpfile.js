8107a004443952aac80a84fd322bba957e530128
Antonio Jesús Navarro Expósito <Antonio Jesus Navarro Exposito>
2018-09-18 13:53:21 +0200

    memoryGame de cartas

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 var autoPrefixer = require ('gulp-autoprefixer');
 
 
@@ -16,5 +18,19 @@ gulp.task('watch-sass', function() {
    gulp.watch("./sass/**/*.sass", ['sass']);
   });
 
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+  gulp.watch('./sass/**/*.sass', ['sass']);
+  gulp.watch('index.html').on('change', browserSync.reload);
+  gulp.watch('stylesheets/style.css').on('change', browserSync.reload);
+  gulp.watch('src/memory.js').on('change', browserSync.reload);
+  gulp.watch('src/main.js').on('change', browserSync.reload);
+});
+
 //Vuelve a ejecutar la tarea cuando se modifica algún archivo
-gulp.task('default', ['watch-sass']);
+gulp.task('default', ['server']);
--- src/main.js
+++ src/main.js
@@ -1,49 +1,49 @@
 var cards = [
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' },
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' }
+  { name: '2DePicas',   img: '2DePicas.png' },
+  { name: '2DePicas',   img: '2DePicas.png' },
+  { name: '3DePicas',   img: '3DePicas.png' },
+  { name: '3DePicas',   img: '3DePicas.png' },
+  { name: '4DePicas',   img: '4DePicas.png' },
+  { name: '4DePicas',   img: '4DePicas.png' },
+  { name: '5DePicas',   img: '5DePicas.png' },
+  { name: '5DePicas',   img: '5DePicas.png' },
+  { name: '6DePicas',   img: '6DePicas.png' },
+  { name: '6DePicas',   img: '6DePicas.png' },
+  { name: '7DePicas',   img: '7DePicas.png' },
+  { name: '7DePicas',   img: '7DePicas.png' },
+  { name: '8DePicas',   img: '8DePicas.png' },
+  { name: '8DePicas',   img: '8DePicas.png' },
+  { name: '9DePicas',   img: '9DePicas.png' },
+  { name: '9DePicas',   img: '9DePicas.png' },
+  { name: '10DePicas',  img: '10DePicas.png' },
+  { name: '10DePicas',  img: '10DePicas.png' },
+  { name: 'JDePicas',   img: 'JDePicas.png' },
+  { name: 'JDePicas',   img: 'JDePicas.png' },
+  { name: 'QDePicas',   img: 'QDePicas.png' },
+  { name: 'QDePicas',   img: 'QDePicas.png' },
+  { name: 'AsDePicas',  img: 'AsDePicas.png' },
+  { name: 'AsDePicas',  img: 'AsDePicas.png' }
 ];
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   memoryGame.shuffleCard(cards);
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
-    html += '<div class= "card" id="card_' + pic.name + '">';
-    html += '<div class="back"';
+    html += '<div class= "card" class="card__' + pic.name + '">';
+    html += '<div class="card__back"';
     html += '    name="'       + pic.img +  '">';
     html += '</div>';
-    html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + ') no-repeat">';
+    html += '<div class="card__front">';
+    html += '<img style="background: url(img/' + pic.img + ') no-repeat">';
     html += '</div>';
     html += '</div>';
   });
-
   // Add all the div's to the HTML
-  document.querySelector('.memory-board').innerHTML = html;
+  document.querySelector('.memory-board__inner').innerHTML = html;
   // Bind the click event of each element to a function
 
-$('.back').on('click', function () {
+$('.card__back').on('click', function () {
+
     $(this).toggle();
   	$(this).siblings().toggle();
 
@@ -56,7 +56,7 @@ $('.back').on('click', function () {
       $('.score__pairs-clicked').text(memoryGame.pairsClicked);
       $('.score__pairs-guessed').text(memoryGame.pairsGuessed);
 
-      $('.back').addClass('blocked');
+      $('.card__back').addClass('blocked');
 
       if(!res) {
         setTimeout(function () {
@@ -65,11 +65,11 @@ $('.back').on('click', function () {
           memoryGame.currentPair[1].toggle();
           memoryGame.currentPair[1].siblings().toggle();
           memoryGame.currentPair = [];
-          $('.back').removeClass('blocked');
+          $('.card__back').removeClass('blocked');
         },1000) }
       else {
           memoryGame.currentPair = [];
-          $('.back').removeClass('blocked');
+          $('.card__back').removeClass('blocked');
         }
       }
       if (memoryGame.finished()) {
